5ae0e856376c0cd0e60b34d6464a20751397a13e
L0L022 <L0L022@openmailbox.org>
2017-06-23 16:41:39 +0200

    son nul

--- game/world.gd
+++ game/world.gd
@@ -2,10 +2,17 @@ extends Node2D
 
 var player = null
 var song_player = null
+var song_anim = null
+
+const STATE_UP = 0
+const STATE_LOW = 1
+var state = -1
+var last_song = ""
 
 func _ready():
 	player = get_node("player")
 	song_player = get_node("sounds/song_player")
+	song_anim = get_node("sounds/song_anim")
 
 func _on_jungle_body_enter(body):
 	play_song(body, "jungle")
@@ -32,5 +39,18 @@ func _on_void_body_enter(body):
 
 func play_song(body, name):
 	if (body extends preload("res://player/player.gd")):
-		song_player.stop_all()
 		song_player.play(name)
+#		last_song = name
+#		state = STATE_LOW
+#		song_anim.play("low")
+
+func _on_song_anim_finished():
+	pass
+#	print(state)
+#	if state == STATE_LOW:
+#		song_player.stop_all()
+#		song_player.play(last_song)
+#		print("play: ", last_song)
+#		
+#		state = STATE_UP
+#		song_anim.play("up")
