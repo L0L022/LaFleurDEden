ca177c332407486605d8d96fef57fa197d2cf61f
L0L022 <L0L022@openmailbox.org>
2017-06-25 14:04:25 +0200

    fix bug

--- game/neo/neo.gd
+++ game/neo/neo.gd
@@ -10,14 +10,14 @@ func _ready():
 	if int(rand_range(0, 2)) == 0:
 		set_scale(get_scale() * Vector2(-1, 1))
 	
-	_on_timer_timeout()
-
-func _on_timer_timeout():
-	anim.stop_all()
-	var anims = anim.get_animation_list()
-	anim.play(anims[int(randf()*10)%anims.size()])
+	_on_anim_timer_timeout()
 
 func _on_neo_body_enter(body):
 	if (not happy and body extends preload("res://heart/heart.gd")):
 		happy = true
 		print("happy")
+
+func _on_anim_timer_timeout():
+	anim.stop_all()
+	var anims = anim.get_animation_list()
+	anim.play(anims[int(randf()*10)%anims.size()])
